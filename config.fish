1fb949ea9146da17acbc82eab72e3fd390857cbf
fr-wawan <hermawantan12@gmail.com>
2025-10-13 18:19:29 +0800

    feat: fish config

---
+++ config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -g fish_greeting
+
+alias ls='exa --icons -a'
+alias a='ddev php artisan'
+alias npr='npm run dev'
+alias dnpr='ddev npm run dev'
