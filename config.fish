0b984d6d11e94ac6eb20e83aa31fad36c57631f5
Wawan Cihuy <hermawantan12@gmail.com>
2026-03-11 21:28:55 +0800

    feat: one dark theme

--- config.fish
+++ config.fish
@@ -1,10 +1,19 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+# Commands to run in interactive sessions can go here
 end
 
 set -g fish_greeting
 
-alias ls='exa --icons -a'
-alias a='ddev php artisan'
+alias ls='eza --icons -a'
+function a
+    if command -q ddev; and test -f .ddev/config.yaml
+        ddev php artisan $argv
+    else
+        php artisan $argv
+    end
+end
 alias npr='npm run dev'
 alias dnpr='ddev npm run dev'
+
+# opencode
+fish_add_path /home/exwadia/.opencode/bin
