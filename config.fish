203e7c4d1c35198c0a73427065a048320cbd7b37
Hermawan Tan <hermawantan12@gmail.com>
2026-09-06 17:54:35 +0800

    feat: refine tmux, kitty, rofi,sway etc

--- config.fish
+++ config.fish
@@ -1,19 +1,20 @@
 if status is-interactive
-# Commands to run in interactive sessions can go here
+    # Commands to run in interactive sessions can go here
 end
 
 set -g fish_greeting
 
 alias ls='eza --icons -a'
-function a
-    if command -q ddev; and test -f .ddev/config.yaml
-        ddev php artisan $argv
-    else
-        php artisan $argv
-    end
-end
+alias a='php artisan'
 alias npr='npm run dev'
 alias dnpr='ddev npm run dev'
+alias dcu='export GIT_MAIN_DIR=~/Projects/hh-server/ && devcontainer up --workspace-folder .'
+alias dcb='devcontainer exec --workspace-folder . bash'
+alias vim='nvim'
+
+# Added by `rbenv init` on Sat Jul  4 12:04:49 PM WITA 2026
+status --is-interactive; and rbenv init - --no-rehash fish | source
+direnv hook fish | source
 
 # opencode
 fish_add_path /home/exwadia/.opencode/bin
